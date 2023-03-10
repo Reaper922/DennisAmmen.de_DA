c605cc79456e1ff5e9c8d4225d4b09f952b4b5e9
Dennis Ammen <dennis.ammen@outlook.com>
2023-03-10 08:15:37 +0100

    Added background image with animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dennisammen.de';
+export class AppComponent implements OnInit {
+  imagesArr = ['code_editor.jpg', 'coding_at_night.jpg', 'laptop.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.imagesArr.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 1);
+    }, 10000);
+  }
 }
